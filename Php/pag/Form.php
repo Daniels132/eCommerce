51b23b12cd85ded2d5eac72e2eb8255d86fea11e
Daniel <Junid2@gmail.com>
2020-06-01 21:35:33 -0300

    Alteração na pagina de cadastro

--- Php/pag/Form.php
+++ Php/pag/Form.php
@@ -1,23 +1,25 @@
 <?php
   session_start();
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Cadastro</title>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
-<body>
-  <a href="../index.php"><img src="../Imagens/logo.png" height="100px" class="offset-md-5"></a>
-    <?php
+ <!DOCTYPE html>
+ <html lang="pt-br">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+<link rel="stylesheet" href="../CSS/Cadastro.css">
+
+ </head>
+ <body>
+	<div class="wrapper fadeInDown"><!--Div da animação -->
+ <?php
        if(isset($_SESSION['status_cadastro'])):
     ?>
-  <div class="border border-dark w-50 p-3 col-md-6 offset-md-3 progress-bar progress-bar-striped bg-success">
+  <div class="border border-dark w-50 p-3 col-md-4 offset-md-4 progress-bar progress-bar-striped bg-success">
     <p class="text-center"><strong>cadastro efetuado</strong></p>
     <p class="text-center"><strong>faça o login informando seu cpf e senha <a href="login.php">aqui</a></strong></p>
   </div>
@@ -28,7 +30,7 @@
     <?php
       if(isset($_SESSION['email_existe'])):
     ?>
-  <div class="border border-dark w-50 p-3 col-md-6 offset-md-3 progress-bar progress-bar-striped bg-danger">
+  <div class="border border-dark w-50 p-3 col-md-5 offset-md-4 progress-bar progress-bar-striped bg-danger">
     <p class="text-center"><strong>O email cadastrado está em uso, por favor insira outro.</strong></p>
   </div>
     <?php
@@ -36,46 +38,82 @@
       unset($_SESSION['email_existe'])
     ?>
 
-<form action="cad.php" method="POST">
-  <div class="border border-info w-50 p-3 col-md-6 offset-md-3">
-    <div class="col-md-6 offset-md-3">
-      <label for="nome">Nome</label>
-     <input type="text" name="nome" class="form-control" id="nome" aria-describedby="nome" placeholder="Seu Nome">
-  </div>
-  <div class="col-md-6 offset-md-3">
-    <label>Telefone</label>
-    <input type="number" name="tel" class="form-control" aria-describedby="tel" placeholder="Apenas números" style="width: 250px;">
-  </div>
-  <div class="col-md-6 offset-md-3">
-    <label for="email">Endereço de email</label>
-    <input type="email" name="email" class="form-control" id="email" aria-describedby="email" placeholder="Seu email">
-    <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
-  </div>
-  <div class="col-md-6 offset-md-3">
-    <label for="cpf">CPF</label>
-    <input type="number" name="cpf" class="form-control" id="cpf" aria-describedby="cpf" placeholder="Apenas números" style="width: 150px;">
-  </div>
-  <div class="col-md-6 offset-md-3">
-  <label for="rg" >RG</label>
-    <input type="number" name="rg" class="form-control" id="rg" aria-describedby="rg" placeholder="Apenas números" style="width: 150px;">
-  </div>
-  <div class="col-md-6 offset-md-3">
-    <label for="end">Endereço</label>
-    <input type="text" name="end" class="form-control" id="end" aria-describedby="end" placeholder="Endereço e número">
-    <input type="text" name="cid" class="form-control float-left" id="cid" aria-describedby="cid" placeholder="Cidade" style="width: 150px;">
-    <input type="text" name="est" class="form-control float-rigth" id="est" aria-describedby="est" placeholder="Estado" style="width: 100px;">
-  </div>
-  <div class="col-md-6 offset-md-3 required">
-    <label for="senha">Senha</label>
-    <input type="password" name="senha" class="form-control" id="senha" placeholder="Senha" style="width: 250px;">
-  </div><br>
-  <div class="col-md-6 offset-md-3">
-  <button type="submit" class="btn btn-success">Cadastrar</button>
-  <a href="../index.php"><button type="button" class="btn btn-danger">Cancelar</button></a>
-  </div>
-  </div>
-</form>
 
-  
-</body>
-</html>
+<div class="container fadeInDown">
+
+<div class="row justify-content-center">
+<div class="col-md-6">
+<div class="card">
+<header class="card-header">
+	<a href="login.php" class="float-right btn btn-outline-primary mt-1">Log in</a>
+	<h4 class="card-title mt-2">Cadastre-se</h4>
+</header>
+<article class="card-body">
+<form action="cad.php" method="POST">
+	<div class="form-row">
+		<div class="col form-group">
+			<label>Nome </label>   
+		  	<input type="text" class="form-control" placeholder="Seu nome" name="nome">
+		</div> <!-- form-group end.// -->
+		<div class="col form-group">
+			<label>Telefone</label>
+		  	<input type="number" class="form-control" placeholder="Apenas numeros" name="tel">
+		</div> <!-- form-group end.// -->
+	</div> <!-- form-row end.// -->
+	<div class="form-group">
+		<label>Email </label>
+		<input type="email" class="form-control" placeholder="example@hotmail.com" name="email">
+		<small class="form-text text-muted">Nunca iremos compartilhar seu email com ninguém</small>
+	</div> <!-- form-group end.// -->
+    <div class="form-row">
+		<div class="col form-group">
+			<label>CPF </label>   
+		  	<input type="Number" class="form-control" placeholder="Apenas numeros" name="cpf">
+		</div> <!-- form-group end.// -->
+		<div class="col form-group">
+			<label>RG</label>
+		  	<input type="number" class="form-control" placeholder="Apenas numeros" name="rg">
+		</div> <!-- form-group end.// -->
+	</div> <!-- form-row end.// -->
+    <div class="form-group">
+		<label>Endereço </label>
+		<input type="text" class="form-control" placeholder="Endereço e numero" name="end">
+	</div> <!-- form-group end.// -->
+    
+	
+	<div class="form-row">
+		<div class="form-group col-md-6">
+		  <label>Cidade</label>
+		  <input type="text" class="form-control" name="cid">
+		</div> <!-- form-group end.// -->
+		<div class="form-group col-md-6">
+		  <label>Estado</label>
+          <input type="text" class="form-control" name="est"> 
+		</div> <!-- form-group end.// -->
+	</div> <!-- form-row.// -->
+	<div class="form-group">
+		<label>Senha</label>
+	    <input class="form-control" type="password" name="senha">
+	</div> <!-- form-group end.// -->  
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-block"> Cadastrar </button>
+    </div> <!-- form-group// -->     
+    <div class="form-group">
+    <a href="../index.php"><button type="button" class="btn btn-primary btn-block"> Cancelar </button></a>
+    </div> <!-- form-group// -->                                                                        
+</form>
+</article> <!-- card-body end .// -->
+<div class="border-top card-body text-center">Já tem uma conta ?<a href="login.php">Log In</a></div>
+</div> <!-- card.// -->
+</div> <!-- col.//-->
+
+</div> <!-- row.//-->
+
+
+</div>
+	</div> <!--Div da animação.//-->
+<!--container end.//-->
+
+     
+ </body>
+ </html>
