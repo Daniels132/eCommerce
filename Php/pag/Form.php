a44c7133e3883a5018f8260d7cde2ebaa5b66fbe
RafaelrMelo <rafaelrodmelo26@gmail.com>
2020-06-07 14:34:42 -0300

    atualização no campo de cadastros

--- Php/pag/Form.php
+++ Php/pag/Form.php
@@ -50,7 +50,16 @@ session_start();
 		endif;
 		unset($_SESSION['cpf_existe'])
 		?>
-
+		<?php
+		if (isset($_SESSION['campoVazio'])) :
+		?>
+			<div class="border border-dark w-50 p-3 col-md-5 offset-md-4 progress-bar progress-bar-striped bg-danger">
+				<p class="text-center"><strong>Preencha todos os campos</strong></p>
+			</div>
+		<?php
+		endif;
+		unset($_SESSION['campoVazio']);
+		?>
 
 		<div class="container fadeInDown">
 
@@ -66,56 +75,65 @@ session_start();
 								<div class="form-row">
 									<div class="col form-group">
 										<label>Nome </label>
-										<input type="text" class="form-control" placeholder="Seu nome" name="nome">
+										<input type="text" class="form-control" placeholder="Seu nome" name="nome" onblur="checar(this,'nome')">
+										<small id="nome"></small>
 									</div> <!-- form-group end.// -->
 									<div class="col form-group">
 										<label>Telefone</label>
-										<input type="number" class="form-control" placeholder="Apenas numeros" name="tel">
+										<input type="number" class="form-control" placeholder="Apenas numeros" name="telefone" onblur="checar(this,'telefone')">
+										<small id="telefone"></small>
 									</div> <!-- form-group end.// -->
 								</div> <!-- form-row end.// -->
 								<div class="form-group">
 									<label>Email </label>
-									<input type="email" class="form-control" placeholder="example@hotmail.com" name="email">
+									<input type="email" class="form-control" placeholder="example@hotmail.com" name="email" onblur="checar(this,'email')">
+									<small id="email"></small>
 									<small class="form-text text-muted">Nunca iremos compartilhar seu email com ninguém</small>
 								</div> <!-- form-group end.// -->
 								<div class="form-row">
 									<div class="col form-group">
 										<label>CPF </label>
-										<input type="Number" class="form-control" placeholder="Apenas numeros" name="cpf">
+										<input type="Number" class="form-control" placeholder="Apenas numeros" name="cpf" onblur="checar(this,'cpf')">
+										<small id="cpf"></small>
 									</div> <!-- form-group end.// -->
 									<div class="col form-group">
 										<label>RG</label>
-										<input type="number" class="form-control" placeholder="Apenas numeros" name="rg">
+										<input type="number" class="form-control" placeholder="Apenas numeros" name="rg" onblur="checar(this,'rg')">
+										<small id="rg"></small>
 									</div> <!-- form-group end.// -->
 								</div>
 
 								<div class="form-group">
 									<label>CEP</label>
 									<d id="cep">
-										<input type="number" class="form-control" placeholder="CEP" name="cep" min="1" max="8"><span onclick="busca()"></span>
+										<input type="number" class="form-control" placeholder="CEP" name="cep"><span onclick="busca()"></span>
 									</d>
 									<small id="ceperro"></small>
 								</div>
 								<!-- form-row end.// -->
 								<div class="form-group">
 									<label>Endereço </label>
-									<input type="text" class="form-control" placeholder="Endereço e numero" name="end" disabled>
+									<input type="text" class="form-control" placeholder="Endereço e numero" name="endereco" disabled onblur="checar(this,'endereco')">
+									<small id="endereco"></small>
 								</div> <!-- form-group end.// -->
-								
+
 
 								<div class="form-row">
 									<div class="form-group col-md-6">
 										<label>Cidade</label>
-										<input type="text" class="form-control" name="cid" disabled>
+										<input type="text" class="form-control" name="cidade" disabled onblur="checar(this,'cidade')">
+										<small id="cidade"></small>
 									</div> <!-- form-group end.// -->
 									<div class="form-group col-md-6">
 										<label>Estado</label>
-										<input type="text" class="form-control" name="est" maxlength="2" disabled>
+										<input type="text" class="form-control" name="estado" maxlength="2" disabled onblur="checar(this,'estado')">
+										<small id="estado"></small>
 									</div> <!-- form-group end.// -->
 								</div> <!-- form-row.// -->
 								<div class="form-group">
 									<label>Senha</label>
-									<input class="form-control" type="password" name="senha">
+									<input class="form-control" type="password" name="senha" onblur="checar(this,'senha')">
+									<small id="senha"></small>
 								</div> <!-- form-group end.// -->
 								<div class="form-group">
 									<button type="submit" class="btn btn-primary btn-block"> Cadastrar </button>
@@ -136,8 +154,7 @@ session_start();
 	</div>
 	<!--Div da animação.//-->
 	<!--container end.//-->
-
-	<script src="../js/cep.js"></script>
+	<script src="../js/cadastro.js"></script>
 </body>
 
 </html>
--- Php/pag/cad.php
+++ Php/pag/cad.php
@@ -1,37 +1,46 @@
 <?php
 session_start();
 include('conexao.php');
-
-$nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
-$telefone = mysqli_real_escape_string($conexao,trim($_POST['tel']));
-$email = mysqli_real_escape_string($conexao, trim($_POST['email']));
-$cpf = mysqli_real_escape_string($conexao, trim($_POST['cpf']));
-$rg = mysqli_real_escape_string($conexao, trim($_POST['rg']));
-$end = mysqli_real_escape_string($conexao, trim($_POST['end']));
-$est = mysqli_real_escape_string($conexao, trim($_POST['est']));
-$cid = mysqli_real_escape_string($conexao, trim($_POST['cid']));
-$senha = mysqli_real_escape_string($conexao, trim($_POST['senha']));
-$sql = "select count(*) as total from clientes where email = '$email'";
-$result = mysqli_query($conexao, $sql);
-$row = mysqli_fetch_assoc($result);
-if($row['total'] == 1){
-   $_SESSION['email_existe'] =  true;
-    header('location: Form.php');
-    exit;
-}
-$sql = "select count(*) as total from clientes where cpf = '$cpf'";
-$result = mysqli_query($conexao, $sql);
-$row = mysqli_fetch_assoc($result);
-if($row['total'] == 1){
-   $_SESSION['cpf_existe'] =  true;
-    header('location: Form.php');
-    exit;
-}
-$sql = "INSERT INTO clientes (nome, telefone, email, cpf, rg, endereço, cidade, estado, senha)
+if (
+    !empty($_POST['nome']) && !empty($_POST['telefone']) && !empty($_POST['email']) && !empty($_POST['cpf']) && !empty($_POST['rg']) &&
+    !empty($_POST['endereco']) && !empty($_POST['estado']) && !empty($_POST['cidade']) && !empty($_POST['senha'])
+) {
+    $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
+    $telefone = mysqli_real_escape_string($conexao, trim($_POST['telefone']));
+    $email = mysqli_real_escape_string($conexao, trim($_POST['email']));
+    $cpf = mysqli_real_escape_string($conexao, trim($_POST['cpf']));
+    $rg = mysqli_real_escape_string($conexao, trim($_POST['rg']));
+    $end = mysqli_real_escape_string($conexao, trim($_POST['endereco']));
+    $est = mysqli_real_escape_string($conexao, trim($_POST['estado']));
+    $cid = mysqli_real_escape_string($conexao, trim($_POST['cidade']));
+    $senha = mysqli_real_escape_string($conexao, trim($_POST['senha']));
+    $sql = "select count(*) as total from clientes where email = '$email'";
+    $result = mysqli_query($conexao, $sql);
+    $row = mysqli_fetch_assoc($result);
+    if ($row['total'] == 1) {
+        $_SESSION['email_existe'] =  true;
+        header('location: Form.php');
+        exit;
+    }
+    $sql = "select count(*) as total from clientes where cpf = '$cpf'";
+    $result = mysqli_query($conexao, $sql);
+    $row = mysqli_fetch_assoc($result);
+    if ($row['total'] == 1) {
+        $_SESSION['cpf_existe'] =  true;
+        header('location: Form.php');
+        exit;
+    }
+    $sql = "INSERT INTO clientes (nome, telefone, email, cpf, rg, endereço, cidade, estado, senha)
 VALUES ('$nome', '$telefone', '$email', '$cpf', '$rg', '$end', '$est', '$cid', md5('$senha'))";
-if($conexao->query($sql)=== TRUE){
-    $_SESSION['cpf'] = $cpf;
-    $_SESSION['senha']=$senha;
-    header('Location: lg.php');
+    if ($conexao->query($sql) === TRUE) {
+        $_SESSION['cpf'] = $cpf;
+        $_SESSION['senha'] = $senha;
+        header('Location: lg.php');
+        exit();
+    }
+}
+else{
+    $_SESSION['campoVazio']=true;
+    header("Location:Form.php");
     exit();
 }
